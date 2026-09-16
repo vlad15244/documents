ffe56f8a21e4ab14e05dd36100e73bb21010b54f
vlad <vladislav.masentsov@yandex.ru>
2026-09-16 20:29:32 +0300

    excel

--- excel.js
+++ excel.js
@@ -7,7 +7,19 @@ async function ExportToLogin(table, rows){
     // Лист
     const worksheet = workbook.addWorksheet('Заявки');
 
-    
+    let fields = []
+    // Формируем массив с настрйоками Excel из таблицы SQL
+    table.columns.forEach(col => {
+        let headers = { header:col.ToString('', false), key: col.ToString('', false), size : 15};
+        fields.push(headers);
+    });
+
+    worksheet.columns = fields.map(field => ({
+            header: field.header,
+            key: field.key,
+            size: field.size        
+        }));
+
 
 
 }
